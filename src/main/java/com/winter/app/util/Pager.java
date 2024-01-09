199c406ea065c82385d35a0de91d669ec14bfd66
Nohnon <arark057@gmail.com>
2024-01-09 17:40:14 +0900

    1/9

---
+++ src/main/java/com/winter/app/util/Pager.java
@@ -0,0 +1,11 @@
+package com.winter.app.util;
+
+public class Pager {
+	public void makenum() {
+		//1. 총 갯수로 총 페이지 수 구하기
+		//2. 총 페이지수로 총 블럭수 구하기
+		//3.현재 페이지 번호로 현재블럭 번호 구하기
+		//4. 현재 블럭 번호로 시작번호와 끝 번호 구하기
+		//5. 이전, 다음 블럭 유무 구하기
+	}
+}
