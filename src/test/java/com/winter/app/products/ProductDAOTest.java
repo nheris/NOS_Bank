208f1f68f3438b5fc0717e84e59aea8c2a5171f2
Nohnon <arark057@gmail.com>
2024-01-11 17:50:16 +0900

    ...

--- src/test/java/com/winter/app/products/ProductDAOTest.java
+++ src/test/java/com/winter/app/products/ProductDAOTest.java
@@ -1,5 +1,7 @@
 package com.winter.app.products;
 
+import java.util.Random;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,8 +15,19 @@ public class ProductDAOTest extends MyTest{
 	public void addTest() throws Exception{
 		ProductDTO productDTO = new ProductDTO();
 		
+//		productDTO.setProductName("Test");
+//		productDTO.setProductContents("Test Detail");
+//		productDTO.setProductRate(2.9);
+//		productDTO.setProductJumsu(2.7);
+//		int result = productDAO.add(productDTO);
+		
+//		System.out.println(result);
+		
 		for(int i=0; i<100;i++) {
 			productDTO.setProductName("상품"+i);
+			productDTO.setProductContents("Test Detail");
+			productDTO.setProductRate(Math.random()*12);
+			productDTO.setProductJumsu(Math.random()*5);
 			int result = productDAO.add(productDTO);
 			if(i%10==0) {
 				Thread.sleep(500);
